e131b16ab2e9fd1bc041d42557138b73c401fc66
Earl OConnor <earl_oconnor@shi.com>
2023-11-01 15:08:49 -0400

    updates

--- main.py
+++ main.py
@@ -21,3 +21,7 @@ async def read_item(skip: int = 0,limit: int = 10):
 @app.get("/list/{list_item}")
 async def read_item(list_item: int | None = None):
     return fake_items[list_item]
+
+@app.post("/put/{put_item}")
+async def create_item(put_item:str)
+    fake_items["name"].append(put_item)
