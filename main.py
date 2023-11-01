5860d6507c7dbec85c7063020c05e315af9527eb
Earl OConnor <earl_oconnor@shi.com>
2023-11-01 11:18:12 -0400

    update paths

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Creates the root directory, this is required
 @app.get("/")
 async def root():
     return {"message":"Hello World"}
 
+@app.get("/test/{test_id}")
+async def read_item(test_id: int):
+    return {"test_id": test_id}
