a0af87abd5a46e8c67e7343cecb50523725f8f77
Earl OConnor <earl_oconnor@shi.com>
2023-11-03 19:32:35 -0400

    update database files

---
+++ sql_app/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, mapped_column, Mapped
+
+from .database import Base
+
+class User(Base):
+    __tablename__ = "food"
+
+    id: Mapped[Integer] = mapped_column(primary_key=True)
+    name: Mapped[String] = mapped_column()
+    price: Mapped[String] = mapped_column()
+
+class Stores(Base):
+    __tablename__ = "stores"
+
+    id: Mapped[Integer] = mapped_column(primary_key=True)
+    name: Mapped[String] = mapped_column()
+    state: Mapped[String] = mapped_column()
+
+class Join(Base):
+    __tablename__ = "join"
+
+    store_id: Mapped[Integer] = mapped_column(primary_key=True, ForeignKey("stores.id"))
+    food_id: Mapped[Integer] = mapped_column(primary_key=True, ForeignKey("food.id"))
+    inventory: Mapped[Integer] = mapped_column()
