94324309527e605835ae8d5877e2349faad7971f
Earl OConnor <earl_oconnor@shi.com>
2023-11-06 16:50:42 -0500

    testing

--- listCompareDict.py
+++ listCompareDict.py
@@ -56,38 +56,3 @@ def compare_lists(list1,list2):
 
 compare_lists(list1,list2)
 
-# def compare_lists2(list1: list,list2: list) -> bool: 
-
-#     df1 = pd.DataFrame(list1)
-#     df2 = pd.DataFrame(list2)
-#     diff = dataframe_difference(df1,df2)
-#     result = len(diff) == 0
-#     if not result:
-#         print(f'There are {len(diff)} differences: \n {diff.head()}')
-#     return result
-
-# def dataframe_difference(df1: DataFrame, df2: DataFrame) -> DataFrame:
-    
-#     comparison_df = df1.merge(df2, indicator=True, how='outer')
-#     diff_df = comparison_df[comparison_df['merge'] != 'both']
-#     return diff_df
-
-# print(compare_lists2(list1,list2))
-
-df1 = pd.DataFrame(list1)
-df2 = pd.DataFrame(list2)
-# df2.rename(columns={'Title':'labname','asgclod':'status'},inplace=True)
-
-print(df1)
-print('\n')
-print(df2)
-print('\n')
-
-missing_in_df2 = df1.loc[~df1['labname'].isin(df2['Title'])]
-missing_in_df1 = df2.loc[~df2['Title'].isin(df1['labname'])]
-
-print(missing_in_df2)
-print('\n')
-print(missing_in_df1)
-
-print(missing_in_df1['Title'])
