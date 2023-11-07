d509e21099498291e9c128bbf62f4e9ee5fa953d
Earl OConnor <earl_oconnor@shi.com>
2023-11-07 17:58:47 -0500

    fixed spelling errors

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declaratice import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:postgres@127.0.0.1/testdb"
