f305c866b0f2da55f373de1dc79c457b59272f55
Earl OConnor <earl_oconnor@shi.com>
2023-11-08 10:14:43 -0500

    update db user

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:postgres@127.0.0.1/testdb"
+SQLALCHEMY_DATABASE_URL = f"postgresql://earl:labadmin123@127.0.0.1/testdb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
