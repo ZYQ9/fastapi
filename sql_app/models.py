fe3c86ff14e7d0d5308a8087b4564d681333cf34
Earl OConnor <earl_oconnor@shi.com>
2023-11-08 09:57:53 -0500

    fix

--- sql_app/models.py
+++ sql_app/models.py
@@ -6,9 +6,9 @@ from .database import Base
 class Food(Base):
     __tablename__ = "food"
 
-    id: Mapped[Integer] = mapped_column(primary_key=True)
-    name: Mapped[String] = mapped_column()
-    price: Mapped[String] = mapped_column()
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    price: Mapped[str] = mapped_column()
 
     join_food: Mapped[list["Join"]] = relationship(back_populates="food")
 
@@ -16,18 +16,19 @@ class Food(Base):
 class Stores(Base):
     __tablename__ = "stores"
 
-    id: Mapped[Integer] = mapped_column(primary_key=True)
-    name: Mapped[String] = mapped_column()
-    state: Mapped[String] = mapped_column()
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    state: Mapped[str] = mapped_column()
 
     join_stores: Mapped[list["Join"]] = relationship(back_populates="stores")
 
 class Join(Base):
     __tablename__ = "join"
 
-    store_id: Mapped[Integer] = mapped_column( ForeignKey("stores.id"),primary_key=True)
-    food_id: Mapped[Integer] = mapped_column(ForeignKey("food.id"),primary_key=True)
-    inventory: Mapped[Integer] = mapped_column()
+    store_id: Mapped[int] = mapped_column( ForeignKey("stores.id"),primary_key=True)
+    food_id: Mapped[int] = mapped_column(ForeignKey("food.id"),primary_key=True)
+    inventory: Mapped[int] = mapped_column()
 
     food: Mapped[list["Food"]] = relationship(back_populates="join_food")
     stores: Mapped[list["Stores"]] = relationship(back_populates="join_stores")
+
