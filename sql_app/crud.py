ab8df688cc91051ca28e872652443310c21194db
Earl OConnor <earl_oconnor@shi.com>
2023-11-08 11:19:46 -0500

    update crud

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -3,3 +3,10 @@ from . import models, schemas
 
 def get_food(db: Session):
     return db.query(models.Food).all()
+
+def create_food(db: Session, food: schemas.Food):
+    db_food = models.Food(id=food.id, name=food.name, price=food.price)
+    db.add(db_food)
+    db.commit()
+    db.refresh(db_food)
+    return db_food
