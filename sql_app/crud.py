9e861328e01d8c30429ce95d8fc8c45ee4fd6386
Earl OConnor <earl_oconnor@shi.com>
2023-11-08 15:40:22 -0500

    bug fix

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -24,7 +24,7 @@ def get_store_by_id(db: Session, id: int):
 
 
 # Join table functions
-def get_inventory_by_store(db: Session, id: id.int):
+def get_inventory_by_store(db: Session, id: id):
     db_inv = db.query(models.Stores).join(models.Join).filter(models.Stores.id == id).first()
 
     store_resp = schemas.StoreResponse(id=db_inv.id, state=db_inv.state, name=db_inv.name)
