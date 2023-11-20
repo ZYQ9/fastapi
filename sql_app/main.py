e81c551b92dd1c45f3a92340ce7c8d6416183108
Earl OConnor <earl_oconnor@shi.com>
2023-11-20 11:34:58 -0500

    bug fix

--- sql_app/main.py
+++ sql_app/main.py
@@ -30,7 +30,7 @@ def get_db():
 # API request to get food 
 @app.get("/food", response_model=list[schemas.Food])
 async def read_food(
-    token: Annotated[str, Depends(oauth2_scheme)]
+    token: Annotated[str, Depends(oauth2_scheme)],
     db: Session = Depends(get_db)):
     food = crud.get_food(db)
     return food
