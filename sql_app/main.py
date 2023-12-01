46f8dbb2c6dc969f6b55def5ec87e087076c0ff8
ZYQ9 <earloconnor1@gmail.com>
2023-12-01 15:59:51 -0500

    app

--- sql_app/main.py
+++ sql_app/main.py
@@ -41,14 +41,14 @@ instrumentation_key = '1345b0d1-2330-4086-bc37-f378ee010f5a'
 ai_handler= AzureLogHandler(connection_string=f'InstrumentationKey=1345b0d1-2330-4086-bc37-f378ee010f5a;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/')
 
 
-root_logger = logging.getLogger("uvicorn.access")
-root_logger.addHandler(ai_handler)
+#root_logger = logging.getLogger("uvicorn.access")
+#root_logger.addHandler(ai_handler)
 #root_logger.setLevel(logging.INFO)
 
 @app.on_event("startup")
 def startup_event():
 
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger("uvicorn.access")
     logger.addHandler(ai_handler)
 
 
