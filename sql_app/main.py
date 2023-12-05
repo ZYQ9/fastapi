1874f08d119e2cdf6113f555d0f46c8a51451866
ZYQ9 <earloconnor1@gmail.com>
2023-12-05 15:27:09 -0500

    app

--- sql_app/main.py
+++ sql_app/main.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
-
+#this is a comment
 #? OpenTelemetry imports
 
 import logging
