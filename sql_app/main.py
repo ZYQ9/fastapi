fc126ef605190ac845485c578e546fcf722e93cf
ZYQ9 <earloconnor1@gmail.com>
2023-12-13 13:26:20 -0500

    logging

--- sql_app/main.py
+++ sql_app/main.py
@@ -10,7 +10,17 @@ import logging
 import uvicorn
 from azure.monitor.opentelemetry import configure_azure_monitor
 
-configure_azure_monitor(connection_string=f'InstrumentationKey=1345b0d1-2330-4086-bc37-f378ee010f5a')
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry import trace
+# configure_azure_monitor(connection_string=f'InstrumentationKey=1345b0d1-2330-4086-bc37-f378ee010f5a')
+
+exporter = AzureMonitorTraceExporter.from_connection_string(
+    "InstrumentationKey=1345b0d1-2330-4086-bc37-f378ee010f5a"
+)
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(exporter))
 
 logger1 = logging.getLogger('uvicorn.access')
 logger1.setLevel(logging.INFO)
