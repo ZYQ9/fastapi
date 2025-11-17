227cc7f0bfb51ed91fde8cb415ed03af8c6118c5
Earl OConnor <earloconnor1@gmail.com>
2025-11-17 14:04:43 -0500

    Refactor schemas.py by removing unnecessary blank lines for improved readability

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -21,29 +21,24 @@ class UserInDB(User):
 # DB Schemas
 class FoodBase(BaseModel):
     model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
-    
     id: int
     
     
 class Food(FoodBase):
-    
     name: str
     price: str
 
 class StoreBase(BaseModel):
     model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
-    
     id: int 
     
 
 class Store(StoreBase):
-    
     name: str
     state: str
 
 class JoinBase(BaseModel):
     model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
-
     store_id: int
     food_id: int
 
